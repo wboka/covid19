20438216edbc9b594d1f8d04d3ee8f876f20361d
Wayne Boka <boka.wayne+GitHub@gmail.com>
2020-04-02 11:48:31 -0400

    Add toggle to change how death/hospitalized % is calculated

--- components/statsSummary.vue
+++ components/statsSummary.vue
@@ -1,5 +1,20 @@
 <template>
   <div class="stats">
+    <p class="text-center">
+      <button
+        type="button"
+        class="text-white font-bold py-2 px-4 m-4 rounded-full"
+        :class="{
+          'bg-teal-700 hover:bg-teal-500': showPercentFromTotal,
+          'bg-blue-700 hover:bg-blue-500': !showPercentFromTotal
+        }"
+        @click="showPercentFromTotal = !showPercentFromTotal"
+      >
+        Show Death and Hospitalized as Percentage of
+        {{ showPercentFromTotal ? 'Positive' : 'Total' }} Tests
+      </button>
+    </p>
+
     <div class="flex flex-wrap text-center">
       <div class="w-full sm:w-1/3 lg:w-1/5">
         <div class="m-2 text-white bg-green-900 shadow-lg">
@@ -51,7 +66,17 @@
           <div class="text-4xl">
             {{ deaths ? deaths.toLocaleString() : '-' }}
           </div>
-          <div class="text-2xl">{{ ((deaths / total) * 100).toFixed(2) }}%</div>
+          <div class="text-2xl">
+            {{
+              (
+                (deaths / (showPercentFromTotal ? total : positive)) *
+                100
+              ).toFixed(2)
+            }}%
+            <span class="label"
+              >of {{ showPercentFromTotal ? 'total' : 'positive' }} tests</span
+            >
+          </div>
         </div>
       </div>
       <div class="w-full sm:w-1/2 lg:w-1/5">
@@ -63,7 +88,15 @@
             {{ hospitalized ? hospitalized.toLocaleString() : '-' }}
           </div>
           <div class="text-2xl">
-            {{ ((hospitalized / total) * 100).toFixed(2) }}%
+            {{
+              (
+                (hospitalized / (showPercentFromTotal ? total : positive)) *
+                100
+              ).toFixed(2)
+            }}%
+            <span class="label"
+              >of {{ showPercentFromTotal ? 'total' : 'positive' }} tests</span
+            >
           </div>
         </div>
       </div>
@@ -100,7 +133,8 @@ export default {
   data() {
     return {
       dateFormat: 'MMMM dd, yyyy p',
-      format
+      format,
+      showPercentFromTotal: true
     }
   }
 }
--- pages/us/_state.vue
+++ pages/us/_state.vue
@@ -24,81 +24,13 @@
       </p>
     </div>
 
-    <div class="flex flex-wrap text-center">
-      <div class="w-full sm:w-1/3 md:w-1/3 lg:w-1/5">
-        <div class="m-2 text-white bg-green-900 shadow-lg">
-          <h3>
-            Positive Tests
-          </h3>
-          <div class="text-4xl">
-            {{ stats.positive ? stats.positive.toLocaleString() : '-' }}
-          </div>
-          <div class="text-2xl">
-            {{ makePercentage(stats.positive, stats.totalTestResults) }}%
-          </div>
-        </div>
-      </div>
-
-      <div class="w-full sm:w-1/3 md:w-1/3 lg:w-1/5">
-        <div class="m-2 text-white bg-red-800 shadow-lg">
-          <h3>
-            Negative Tests
-          </h3>
-          <div class="text-4xl">
-            {{ stats.negative ? stats.negative.toLocaleString() : '-' }}
-          </div>
-          <div class="text-2xl">
-            {{ makePercentage(stats.negative, stats.totalTestResults) }}%
-          </div>
-        </div>
-      </div>
-
-      <div class="w-full sm:w-1/3 md:w-1/3 lg:w-1/5">
-        <div class="m-2 text-white bg-blue-900 shadow-lg">
-          <h3>
-            Total Tests
-          </h3>
-          <div class="text-4xl">
-            {{
-              stats.totalTestResults
-                ? stats.totalTestResults.toLocaleString()
-                : '-'
-            }}
-          </div>
-          <div class="text-2xl">
-            -
-          </div>
-        </div>
-      </div>
-
-      <div class="w-full sm:w-1/2 md:w-1/2 lg:w-1/5">
-        <div class="m-2 text-white bg-black shadow-lg">
-          <h3>
-            Deaths
-          </h3>
-          <div class="text-4xl">
-            {{ stats.death ? stats.death.toLocaleString() : '-' }}
-          </div>
-          <div class="text-2xl">
-            {{ makePercentage(stats.death, stats.totalTestResults) }}%
-          </div>
-        </div>
-      </div>
-
-      <div class="w-full sm:w-1/2 md:w-1/2 lg:w-1/5">
-        <div class="m-2 text-white bg-blue-600 shadow-lg">
-          <h3>
-            Hospitalized
-          </h3>
-          <div class="text-4xl">
-            {{ stats.hospitalized ? stats.hospitalized.toLocaleString() : '-' }}
-          </div>
-          <div class="text-2xl">
-            {{ makePercentage(stats.hospitalized, stats.totalTestResults) }}%
-          </div>
-        </div>
-      </div>
-    </div>
+    <Stats
+      :deaths="stats.death"
+      :hospitalized="stats.hospitalized"
+      :negative="stats.negative"
+      :positive="stats.positive"
+      :total="stats.totalTestResults"
+    />
 
     <div class="flex flex-wrap">
       <div class="w-full sm:w-1/2 text-center">
