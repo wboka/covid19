3362d32ae6a597fb781150699558cdeebcc9e5f2
wboka <boka.wayne+GitHub@gmail.com>
2020-05-22 22:41:16 -0400

    remove error test

--- __tests__/error.spec.js
+++
@@ -1,7 +0,0 @@
-import { shallowMount } from '@vue/test-utils'
-import { expect } from 'chai'
-import Error from '@/layouts/error.vue'
-
-describe('Error Page', () => {
-  it('should render correctly with a 404', () => {})
-})
