b28b376f33d6cd02fd385637655d78aaf107a4c0
wboka <boka.wayne+GitHub@gmail.com>
2020-05-22 22:07:17 -0400

    add base test file

---
+++ __tests__/error.spec.js
@@ -0,0 +1,7 @@
+import { shallowMount } from '@vue/test-utils'
+import { expect } from 'chai'
+import Error from '@/layouts/error.vue'
+
+describe('Error Page', () => {
+  it('should render correctly with a 404', () => {})
+})
